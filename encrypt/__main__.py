a51545d7959b845500d0351a8a67655f581987ad
Espen Kirkesæther Brun <ekb@horus.no>
2023-02-23 11:11:49 +0100

    Add encryption code

--- encrypt/__main__.py
+++ encrypt/__main__.py
@@ -1,9 +1,33 @@
-from encrypt.encrypt import haversine
+import getpass
+from encrypt.encrypt import encrypt
+from encrypt.encrypt import decrypt
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description="Test")
-    parser.add_argument("input")
-    input = parser.parse_args().input
-    print(input)
-    print(haversine(52.370216, 4.895168, 52.520008, 13.404954))
+    parser = argparse.ArgumentParser(description='File Encryptor Script with a Password')
+    parser.add_argument('file', help='File to encrypt/decrypt')
+    parser.add_argument('-e', '--encrypt', action='store_true',
+                        help='Whether to encrypt the file, only -e or -d can be specified.')
+    parser.add_argument('-d', '--decrypt', action='store_true',
+                        help='Whether to decrypt the file, only -e or -d can be specified.')
+
+    args = parser.parse_args()
+    file = args.file
+
+    if args.encrypt:
+        password = getpass.getpass('Enter the password for encryption: ')
+    elif args.decrypt:
+        password = getpass.getpass('Enter the password you used for encryption: ')
+
+    encrypt_ = args.encrypt
+    decrypt_ = args.decrypt
+
+    if encrypt_ and decrypt_:
+        raise TypeError('Please specify whether you want to encrypt the file or decrypt it.')
+    elif encrypt_:
+        encrypt(file, password)
+    elif decrypt_:
+        decrypt(file, password)
+    else:
+        raise TypeError('Please specify whether you want to encrypt the file or decrypt it.')
+    
--- encrypt/encrypt.py
+++ encrypt/encrypt.py
@@ -1,24 +1,29 @@
-from math import radians, cos, sin, asin, sqrt
+import scrypt
 
-def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
-    """
-    Calculate the great circle distance between two points on the 
-    earth (specified in decimal degrees), returns the distance in
-    meters.
-    All arguments must be of equal length.
-    :param lon1: longitude of first place
-    :param lat1: latitude of first place
-    :param lon2: longitude of second place
-    :param lat2: latitude of second place
-    :return: distance in meters between the two sets of coordinates
-    """
-    # Convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+def encrypt(filename, password):
+    '''
+    Given a filename (str) and password (str), encrypts the file and overwrites it
+    '''
+    with open(filename, 'rb') as file:
+        # read all file data
+        file_data = file.read()
+    # encrypt data
+    encrypted_data = scrypt.encrypt(file_data, password)
+    # write the encrypted file
+    with open(filename, 'wb') as file:
+        file.write(encrypted_data)
 
-    # Haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371 # Radius of earth in kilometers
-    return c * r
+def decrypt(filename, password):
+    '''
+    Given a filename (str) and password (str), decrypts the file and overwrites
+    '''
+    with open(filename, 'rb') as file:
+        # read the encrypted data
+        encrypted_data = file.read()
+    # decrypt data
+    decrypted_data = scrypt.decrypt(encrypted_data, password)
+
+    # write the original file
+    with open(filename, 'w') as file:
+        file.write(decrypted_data)
+    print('File decrypted successfully')
